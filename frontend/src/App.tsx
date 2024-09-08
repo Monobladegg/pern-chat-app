be12cb07ded2e49310264205a64a4e698aef513c
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-09-08 08:03:57 +0300

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+
+function App() {
+
+  return (<div className="p-4 h-screen flex items-center justify-center">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </div>)
+}
+
+export default App
